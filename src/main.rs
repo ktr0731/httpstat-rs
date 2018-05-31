fad6e6101da8f8e29bcc0fc6dcf7725f92089562
ktr0731 <aizu.s1230022@gmail.com>
2018-05-31 21:35:30 +0900

    use curl command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
-extern crate reqwest;
 extern crate url;
 
-use reqwest::Error;
 use std::io::{self, Write};
 use std::{env, process};
 use url::{ParseError, Url};
+
+const CURL_FORMAT: &str = "
+content_type=%{content_type}
+filename_effective=%{filename_effective}
+ftp_entry_path=%{ftp_entry_path}
+http_code=%{http_code}
+http_connect=%{http_connect}
+local_ip=%{local_ip}
+local_port=%{local_port}
+num_connects=%{num_connects}
+num_redirects=%{num_redirects}
+redirect_url=%{redirect_url}
+remote_ip=%{remote_ip}
+remote_port=%{remote_port}
+size_download=%{size_download}
+size_header=%{size_header}
+size_request=%{size_request}
+size_upload=%{size_upload}
+speed_download=%{speed_download}
+speed_upload=%{speed_upload}
+ssl_verify_result=%{ssl_verify_result}
+time_appconnect=%{time_appconnect}
+time_connect=%{time_connect}
+time_namelookup=%{time_namelookup}
+time_pretransfer=%{time_pretransfer}
+time_redirect=%{time_redirect}
+time_starttransfer=%{time_starttransfer}
+time_total=%{time_total}
+url_effective=%{url_effective}
+";
 
 // const HTTPS_TEMPLATE: &str = "
 //   DNS Lookup   TCP Connection   TLS Handshake   Server Processing   Content Transfer
@@ -28,9 +56,6 @@ use url::{ParseError, Url};
 // ";
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    println!("{}", &args[1]);
     process::exit(match run() {
         Ok(_) => 0,
         Err(err) => {
@@ -52,12 +77,20 @@ fn run() -> Result<(), String> {
 }
 
 fn request(url: &str) -> Result<String, String> {
-    reqwest::get(url)
-        .map_err(|e| format!("failed to send request: {}", e))?
-        .text()
-        .map_err(|e| format!("failed: {}", e))
-    // .map(|res| {
-    //     println!("Response: {}", res.status());
-    // });
-    // Ok(0)
+    let out = process::Command::new("curl")
+        .args(&[
+            "-w",
+            CURL_FORMAT,
+            "-D",
+            "tmpd",
+            "-o",
+            "tmpo",
+            "-s",
+            "-S",
+            url,
+        ])
+        .output()
+        .map_err(|e| format!("failed to execute curl: {}", e))?
+        .stdout;
+    Ok(String::from_utf8_lossy(&out).to_string())
 }
