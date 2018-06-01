e45cac33b3c1353c835953397c9d738698352564
ktr0731 <aizu.s1230022@gmail.com>
2018-06-01 14:02:09 +0900

    add Status struct

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,31 @@
+extern crate serde_json;
 extern crate url;
 
+#[macro_use]
+extern crate serde_derive;
+
+use serde_json::{Error, Value};
 use std::io::{self, Write};
 use std::{env, process};
 use url::{ParseError, Url};
 
-const CURL_FORMAT: &str = "
+const CURL_FORMAT: &str = r#"
 {
-    content_type:       %{content_type},
-    filename_effective: %{filename_effective},
-    ftp_entry_path:     %{ftp_entry_path},
-    http_code:          %{http_code},
-    http_connect:       %{http_connect},
-    local_ip:           %{local_ip},
-    local_port:         %{local_port},
-    num_connects:       %{num_connects},
-    num_redirects:      %{num_redirects},
-    redirect_url:       %{redirect_url},
-    remote_ip:          %{remote_ip},
-    remote_port:        %{remote_port},
-    size_download:      %{size_download},
-    size_header:        %{size_header},
-    size_request:       %{size_request},
-    size_upload:        %{size_upload},
-    speed_download:     %{speed_download},
-    speed_upload:       %{speed_upload},
-    ssl_verify_result:  %{ssl_verify_result},
-    time_appconnect:    %{time_appconnect},
-    time_connect:       %{time_connect},
-    time_namelookup:    %{time_namelookup},
-    time_pretransfer:   %{time_pretransfer},
-    time_redirect:      %{time_redirect},
-    time_starttransfer: %{time_starttransfer},
-    time_total:         %{time_total},
-    url_effective:      %{url_effective}
+    "time_namelookup":    %{time_namelookup},
+    "time_connect":       %{time_connect},
+    "time_appconnect":    %{time_appconnect},
+    "time_pretransfer":   %{time_pretransfer},
+    "time_redirect":      %{time_redirect},
+    "time_starttransfer": %{time_starttransfer},
+    "time_total":         %{time_total},
+    "speed_download":     %{speed_download},
+    "speed_upload":       %{speed_upload},
+    "remote_ip":          "%{remote_ip}",
+    "remote_port":        "%{remote_port}",
+    "local_ip":           "%{local_ip}",
+    "local_port":         "%{local_port}"
 }
-";
+"#;
 
 // const HTTPS_TEMPLATE: &str = "
 //   DNS Lookup   TCP Connection   TLS Handshake   Server Processing   Content Transfer
@@ -56,6 +47,23 @@ const CURL_FORMAT: &str = "
 // starttransfer:%s        |
 // total:%s
 // ";
+//
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Status {
+    time_namelookup: f32,
+    time_connect: f32,
+    time_appconnect: f32,
+    time_pretransfer: f32,
+    time_redirect: f32,
+    time_starttransfer: f32,
+    speed_download: f32,
+    speed_upload: f32,
+    remote_ip: String,
+    remote_port: String,
+    local_ip: String,
+    local_port: String,
+}
 
 fn main() {
     process::exit(match run() {
@@ -71,14 +79,14 @@ fn run() -> Result<(), String> {
     match env::args().nth(1) {
         Some(url) => {
             let res = request(&url)?;
-            println!("{}", res);
+            println!("{:?}", res);
             Ok(())
         }
         None => Err(String::from("Usage: httpstat <url>")),
     }
 }
 
-fn request(url: &str) -> Result<String, String> {
+fn request(url: &str) -> Result<Status, String> {
     let out = process::Command::new("curl")
         .args(&[
             "-w",
@@ -94,5 +102,7 @@ fn request(url: &str) -> Result<String, String> {
         .output()
         .map_err(|e| format!("failed to execute curl: {}", e))?
         .stdout;
-    Ok(String::from_utf8_lossy(&out).to_string())
+    let out = serde_json::from_str(&String::from_utf8_lossy(&out))
+        .map_err(|e| format!("failed to marshal response data: {}", e))?;
+    Ok(out)
 }
