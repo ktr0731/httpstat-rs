9f6412fc17b779ef512f8576583f2c76b889e0f3
ktr0731 <aizu.s1230022@gmail.com>
2018-06-01 15:04:52 +0900

    impl Status struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate serde_json;
+extern crate tempfile;
 extern crate url;
 
 #[macro_use]
 extern crate serde_derive;
 
 use serde_json::{Error, Value};
-use std::io::{self, Write};
+use std::fs::File;
+use std::io::{self, Read, Seek, SeekFrom, Write};
 use std::{env, process};
 use url::{ParseError, Url};
 
@@ -27,16 +29,16 @@ const CURL_FORMAT: &str = r#"
 }
 "#;
 
-// const HTTPS_TEMPLATE: &str = "
-//   DNS Lookup   TCP Connection   TLS Handshake   Server Processing   Content Transfer
-// [%s  |     %s  |    %s  |        %s  |       %s  ]
-// |                |               |                   |                  |
-// namelookup:%s      |               |                   |                  |
-// connect:%s     |                   |                  |
-// pretransfer:%s         |                  |
-// starttransfer:%s        |
-// total:%s
-// ";
+const HTTPS_TEMPLATE: &str = "
+  DNS Lookup   TCP Connection   TLS Handshake   Server Processing   Content Transfer
+[%s  |     %s  |    %s  |        %s  |       %s  ]
+|                |               |                   |                  |
+namelookup:%s      |               |                   |                  |
+connect:%s     |                   |                  |
+pretransfer:%s         |                  |
+starttransfer:%s        |
+total:%s
+";
 //
 // const HTTP_TEMPLATE: &str = "
 //   DNS Lookup   TCP Connection   Server Processing   Content Transfer
@@ -57,12 +59,38 @@ struct Status {
     time_pretransfer: f32,
     time_redirect: f32,
     time_starttransfer: f32,
+    time_total: f32,
     speed_download: f32,
     speed_upload: f32,
     remote_ip: String,
     remote_port: String,
     local_ip: String,
     local_port: String,
+
+    #[serde(skip_deserializing)]
+    range_dns: f32,
+    #[serde(skip_deserializing)]
+    range_connection: f32,
+    #[serde(skip_deserializing)]
+    range_ssl: f32,
+    #[serde(skip_deserializing)]
+    range_server: f32,
+    #[serde(skip_deserializing)]
+    range_transfer: f32,
+}
+
+impl Status {
+    fn new(resp: &str) -> Result<Status, String> {
+        let mut status: Status = serde_json::from_str(resp)
+            .map_err(|e| format!("failed to marshal response data: {}", e))?;
+        status.range_dns = status.time_namelookup;
+        status.range_connection = status.time_connect - status.time_namelookup;
+        status.range_ssl = status.time_pretransfer - status.time_connect;
+        status.range_server = status.time_starttransfer - status.time_pretransfer;
+        status.range_transfer = status.time_total - status.time_starttransfer;
+        let status = status;
+        Ok(status)
+    }
 }
 
 fn main() {
@@ -78,8 +106,8 @@ fn main() {
 fn run() -> Result<(), String> {
     match env::args().nth(1) {
         Some(url) => {
-            let res = request(&url)?;
-            println!("{:?}", res);
+            let res = formatResponseText(request(&url)?)?;
+            println!("{}", res);
             Ok(())
         }
         None => Err(String::from("Usage: httpstat <url>")),
@@ -102,7 +130,11 @@ fn request(url: &str) -> Result<Status, String> {
         .output()
         .map_err(|e| format!("failed to execute curl: {}", e))?
         .stdout;
-    let out = serde_json::from_str(&String::from_utf8_lossy(&out))
-        .map_err(|e| format!("failed to marshal response data: {}", e))?;
-    Ok(out)
+    let resp = &String::from_utf8_lossy(&out);
+    println!("{}", resp);
+    Ok(Status::new(resp)?)
+}
+
+fn formatResponseText(status: Status) -> Result<String, String> {
+    Ok("".to_string())
 }
